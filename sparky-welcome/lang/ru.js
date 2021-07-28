4cbdb3716c4f1e80fb72824e1f251f30b5eeb0ac
pavroo <pavroo@onet.eu>
2021-07-28 14:11:12 +0200

    20210728

---
+++ sparky-welcome/lang/ru.js
@@ -0,0 +1,21 @@
+// Russian localization file for main
+// Originally created by Piotr "MoroS" MroЕјek 2018
+// Under the GNU GPL v3
+// Last update 2021/06/24 by pavroo
+dict.entries['main'] = {};
+// main menu
+dict.entries['main']['home'] = "Домашняя страница";
+dict.entries['main']['forums'] = "Форумы";
+dict.entries['main']['wiki'] = "Wiki";
+dict.entries['main']['donate'] = "Пожертвовать";
+dict.entries['main']['rss'] = "RSS";
+dict.entries['main']['appcenter'] = "AppCenter";
+dict.entries['main']['upgrade'] = "Обновление";
+dict.entries['main']['about'] = "About";
+dict.entries['main']['system'] = "Информация о системе";
+dict.entries['main']['welcome'] = "Добро пожаловать в мир Sparky!";
+dict.entries['main']['keep'] = "Оставайтесь на связи с сообществом Sparky:";
+dict.entries['main']['exit'] = "Выход";
+dict.entries['main']['git'] = "Репозиторий Git";
+dict.entries['main']['install'] = "Установка";
+dict.entries['main']['remaster'] = "Резервное копирование";
