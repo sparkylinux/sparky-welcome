85c84e7454dab69ded2ba9e5ed32a8f7382dfc78
pavroo <pavroo@onet.eu>
2022-07-02 19:28:44 +0200

    20220702

---
+++ 7/sparky-welcome/lang/es_ES.js
@@ -0,0 +1,22 @@
+// Spanish (es_ES) localization file for main
+// Originally created by Piotr "MoroS" Mrożek 2018
+// Under the GNU GPL v3
+// Last update 2021/06/24 by pavroo
+// Translated by depaz
+dict.entries['main'] = {};
+// main menu
+dict.entries['main']['home'] = "Página web";
+dict.entries['main']['forums'] = "Foros";
+dict.entries['main']['wiki'] = "Wiki";
+dict.entries['main']['donate'] = "Donar";
+dict.entries['main']['rss'] = "RSS";
+dict.entries['main']['appcenter'] = "AppCenter";
+dict.entries['main']['upgrade'] = "Actualizar";
+dict.entries['main']['about'] = "Acerca de";
+dict.entries['main']['system'] = "Info del sistema";
+dict.entries['main']['welcome'] = "¡Bienvenido al mundo de Sparky!";
+dict.entries['main']['keep'] = "Manténgase en contacto con la comunidad de Sparky:";
+dict.entries['main']['exit'] = "Salir";
+dict.entries['main']['git'] = "Repositorio Git";
+dict.entries['main']['install'] = "Instalación";
+dict.entries['main']['remaster'] = "Copia de seguridad";
