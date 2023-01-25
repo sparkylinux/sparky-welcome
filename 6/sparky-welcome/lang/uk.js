ceb1a4382af55c9562f297fef2a0dc3035598558
pavroo <pavroo@onet.eu>
2023-01-25 16:53:19 +0100

    20230125

---
+++ 6/sparky-welcome/lang/uk.js
@@ -0,0 +1,21 @@
+// Ukrainian localization file for main by XalisRatt
+// Originally created by Piotr "MoroS" Mrożek 2018
+// Under the GNU GPL v3
+// Last update 2023/01/25 by pavroo
+dict.entries['main'] = {};
+// main menu
+dict.entries['main']['home'] = "Домашня сторінка";
+dict.entries['main']['forums'] = "Форуми";
+dict.entries['main']['wiki'] = "Вікі";
+dict.entries['main']['donate'] = "Пожертвувати";
+dict.entries['main']['rss'] = "RSS";
+dict.entries['main']['appcenter'] = "AppCenter";
+dict.entries['main']['upgrade'] = "Оновлення";
+dict.entries['main']['about'] = "Про Sparky";
+dict.entries['main']['system'] = "Інформація про систему";
+dict.entries['main']['welcome'] = "Вітаємо в світі Sparky!";
+dict.entries['main']['keep'] = "Залишайтесь на зв'язку з спільнотою Sparky:";
+dict.entries['main']['exit'] = "Вихід";
+dict.entries['main']['git'] = "Репозиторій Git";
+dict.entries['main']['install'] = "Встановлення";
+dict.entries['main']['remaster'] = "Резервне копіювання";
