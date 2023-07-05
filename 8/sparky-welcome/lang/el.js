67d305d733552d0c6e0b32b37b4aae41dbee1f2a
pavroo <pavroo@onet.eu>
2023-07-05 21:39:26 +0200

    20230701

---
+++ 8/sparky-welcome/lang/el.js
@@ -0,0 +1,21 @@
+// English localization file for main
+// Originally created by Piotr "MoroS" MroÅ¼ek 2018
+// Under the GNU GPL v3
+// Last update 2021/06/24 by pavroo
+dict.entries['main'] = {};
+// main menu
+dict.entries['main']['home'] = "Αρχική σελίδα";
+dict.entries['main']['forums'] = "Forums";
+dict.entries['main']['wiki'] = "Wiki";
+dict.entries['main']['donate'] = "Δωρεές";
+dict.entries['main']['rss'] = "RSS";
+dict.entries['main']['appcenter'] = "Εφαρμογές";
+dict.entries['main']['upgrade'] = "Ενημερώσεις";
+dict.entries['main']['about'] = "Σχετικά";
+dict.entries['main']['system'] = "Πληροφορίες Συστήματος";
+dict.entries['main']['welcome'] = "Καλωσήρθατε στον κόσμο του Sparky!";
+dict.entries['main']['keep'] = "Κρατήστε επαφή με την κοινότητα του Sparky:";
+dict.entries['main']['exit'] = "Έξοδος";
+dict.entries['main']['git'] = "Αποθετήριο Git";
+dict.entries['main']['install'] = "Εγκατάσταση";
+dict.entries['main']['remaster'] = "Αντιγραφα ασφαλείας";
---
+++ 8/sparky-welcome/lang/it.js
@@ -0,0 +1,22 @@
+// File di localizzazione in Italiano per Sparky Welcome 
+// copyright 2022, Yodel Sinatra <mastodon.uno/@YodelSinatra> 2022/05/18
+// Rilasciato sotto licenza GNU GPL v3
+// Last update 2021/06/24 by pavroo
+// Ultimo aggiornamento 2022/05/18 by Yodel Sinatra <mastodon.uno/@YodelSinatra>
+dict.entries['main'] = {};
+// main menu
+dict.entries['main']['home'] = "Home page";
+dict.entries['main']['forums'] = "Forums";
+dict.entries['main']['wiki'] = "Wiki";
+dict.entries['main']['donate'] = "Donazioni";
+dict.entries['main']['rss'] = "RSS";
+dict.entries['main']['appcenter'] = "AppCenter";
+dict.entries['main']['upgrade'] = "Aggiornamento";
+dict.entries['main']['about'] = "About";
+dict.entries['main']['system'] = "System info";
+dict.entries['main']['welcome'] = "Benvenuti nel mondo di Sparky!";
+dict.entries['main']['keep'] = "Resta in contatto con la comunità di Sparky:";
+dict.entries['main']['exit'] = "Esci";
+dict.entries['main']['git'] = "Git repo";
+dict.entries['main']['install'] = "Installazione";
+dict.entries['main']['remaster'] = "Backup";
