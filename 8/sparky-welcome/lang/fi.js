4403938cf8bd6309b0511a8f3fff1236c621dc03
pavroo <pavroo@onet.eu>
2024-07-31 12:06:55 +0200

    20240731

---
+++ 8/sparky-welcome/lang/fi.js
@@ -0,0 +1,22 @@
+// Suomenkielinen lokalisointitiedosto main
+// Alunperin luonut Piotr "MoroS" MroÅ¼ek 2018
+// GNU GPL v3:n alla
+// Viimeisin päivitys 2024/06/08, Pavroo
+dict.entries['main'] = {};
+// päävalikko
+dict.entries['main']['home'] = "Kotisivu";
+dict.entries['main']['forums'] = "Foorumit";
+dict.entries['main']['wiki'] = "Wiki";
+dict.entries['main']['donate'] = "Lahjoita";
+dict.entries['main']['rss'] = "RSS";
+dict.entries['main']['appcenter'] = "AppCenter";
+dict.entries['main']['upgrade'] = "Päivitä";
+dict.entries['main']['about'] = "Tietoja";
+dict.entries['main']['system'] = "Järjestelmätiedot";
+dict.entries['main']['welcome'] = "Tervetuloa Sparky-maailmaan!";
+dict.entries['main']['keep'] = "Pidä yhteyttä Sparky-yhteisöön:";
+dict.entries['main']['exit'] = "Poistu";
+dict.entries['main']['git'] = "Git repo";
+dict.entries['main']['install'] = "Asennus";
+dict.entries['main']['remaster'] = "Varmuuskopiointi";
+dict.entries['main']['sponsoring'] = "Sponsorit";
